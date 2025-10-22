757def89c08982070e81e8e98c0ba22e8ff2e72f
Heitor-Rebs <heitor.rebordoes@gmail.com>
2025-10-21 21:25:19 -0300

    Nível Aventureiro

--- Detetive.c
+++ Detetive.c
@@ -5,39 +5,83 @@
 /* Criando a estrutura dos nós */
 struct No {
     char nome[50];
+    char pista[100];
     struct No* esquerda;
     struct No* direita;
 };
 
-/* Função para criar novo nó */
-struct No* criarSala(char* nome) {
+/* Criando a estrutura das pistas */
+struct Pista {
+    char pista[100];
+    struct Pista* esquerda;
+    struct Pista* direita;
+};
+
+/* Função para criar nó na árvore de pistas */
+struct Pista* criarPista(const char* pista) {
+    struct Pista* novapista = (struct Pista*) malloc(sizeof(struct Pista));
+    strcpy(novapista->pista, pista);
+    novapista->esquerda = NULL;
+    novapista->direita = NULL;
+    return novapista;
+}
+
+/* Inserir pista na BST */
+struct Pista* inserirPista(struct Pista* raiz, char* pista) {
+    if (raiz == NULL)
+        return criarPista(pista);
+
+        if (strcmp(pista, raiz->pista) < 0)
+            raiz->esquerda = inserirPista(raiz->esquerda, pista);
+        else if (strcmp(pista, raiz->pista) > 0)
+            raiz->direita = inserirPista(raiz->direita, pista);
+
+        return raiz;
+}
+
+/* Função para mostrar as pistas coletadas em ordem alfabética */
+void mostrarPistas(struct Pista* raiz) {
+    if (raiz != NULL) {
+        mostrarPistas(raiz->esquerda);
+        printf("%s\n", raiz->pista);
+        mostrarPistas(raiz->direita);
+    }
+}
+
+/* Função para criar nova sala */
+struct No* criarSala(char* nome, char* pista) {
     struct No* novaSala = (struct No*) malloc(sizeof(struct No));
     strcpy(novaSala->nome, nome);
+    strcpy(novaSala->pista, pista);
     novaSala->esquerda = NULL;
     novaSala->direita = NULL;
     return novaSala;
 }
 
 /* Função para o usuário explorar o mapa */
-void explorarSalas(struct No* raiz, char salasPassadas[][50], int *passos)
+void explorarSalas(struct No* raiz, char salasPassadas[][50], int *passos, struct Pista* pistas)
 {
     int i = 0;
     char lado[3];
     /* Conferir se o comodo existe */
     if (raiz != NULL)
     {
+        inserirPista(pistas,raiz->pista);
         /* Conferir se o comodo tem saida */
         if(raiz->esquerda == NULL && raiz->direita == NULL)
         {
             strcpy(salasPassadas[*passos], raiz->nome);
             (*passos)++;
+            printf("\nSala final: %s\n",raiz->nome);
+            printf("Pistas na sala: %s\n",raiz->pista);
             printf("Comodo sem saidas\n");
         }else
         {
             strcpy(salasPassadas[*passos], raiz->nome);
             (*passos)++;
-            printf("--Explorar mansao--\n");
+            printf("\n--Explorar mansao--\n");
             printf("Sala atual: %s\n",raiz->nome);
+            printf("Pistas na sala: %s\n",raiz->pista);
             printf("d - Direita\n");
             printf("e - Esquerda\n");
             printf("s - Sair\n");
@@ -49,10 +93,12 @@ void explorarSalas(struct No* raiz, char salasPassadas[][50], int *passos)
                 return;
             }else if(strcmp(lado, "d") == 0)
             {
-                explorarSalas(raiz->direita, salasPassadas, passos);
+                explorarSalas(raiz->direita, salasPassadas, passos, pistas);
+
             }else if(strcmp(lado, "e") == 0)
             {
-                explorarSalas(raiz->esquerda, salasPassadas, passos);
+                explorarSalas(raiz->esquerda, salasPassadas, passos, pistas);
+
             }else
             {
                 printf("Opcao Invalida");
@@ -65,16 +111,18 @@ int main()
 {
     char salasPassadas[10][50];
     int passos = 0, i = 0;
-    struct No* raiz = criarSala("Hall de Entrada");
-    raiz->esquerda = criarSala("Sala de Estar");
-    raiz->direita = criarSala("Cozinha");
-    raiz->direita->esquerda = criarSala("Lavanderia");
-    raiz->direita->direita = criarSala("Deposito");
-    raiz->esquerda->esquerda = criarSala("Quarto de hospedes");
-    raiz->esquerda->direita = criarSala("Quarto Principal");
-    raiz->esquerda->direita->direita = criarSala("Banheiro");
-    raiz->esquerda->direita->esquerda = criarSala("Closet");
-    explorarSalas(raiz, salasPassadas, &passos);
+    struct Pista* pistas = criarPista("Pegadas de barro no tapete");
+    struct No* raiz = criarSala("Hall de Entrada", "Pegadas de barro no tapete");
+    raiz->esquerda = criarSala("Sala de Estar", "Copo quebrado e um lenço de seda caído");
+    raiz->direita = criarSala("Cozinha", "Chaleira ainda quente e uma xicara pela metade");
+    raiz->direita->esquerda = criarSala("Lavanderia", "Camisa manchada de sangue na maquina de lavar");
+    raiz->direita->direita = criarSala("Deposito", "Caixa derrubada e lanterna apagada no chão");
+    raiz->esquerda->esquerda = criarSala("Quarto de hospedes", "Janela aberta e pedaço de tecido preso no trinco");
+    raiz->esquerda->direita = criarSala("Quarto Principal", "Gaveta arrombada e joia faltando.");
+    raiz->esquerda->direita->direita = criarSala("Banheiro", "Luvas molhadas sobre a pia");
+    raiz->esquerda->direita->esquerda = criarSala("Closet", "Frasco de veneno vazio em meio as roupas");
+
+    explorarSalas(raiz, salasPassadas, &passos, pistas);
     /* Mostrar os comodos passados */
     if(passos == 0)
     {
@@ -86,6 +134,9 @@ int main()
         {
             printf("Sala %d: %s\n",i+1,salasPassadas[i]);
         }
+
+        printf("\n--Pistas coletadas--\n");
+        mostrarPistas(pistas);
     }
     return;
 
